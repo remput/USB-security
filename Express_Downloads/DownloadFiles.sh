5efb46b39a6fad73109351f6e9d42d66a5bb254c
remput <57086139+remput@users.noreply.github.com>
2020-02-09 23:55:39 +0800

    Add files via upload

--- Express_Downloads/DownloadFiles.sh
+++ Express_Downloads/DownloadFiles.sh
@@ -4,9 +4,7 @@ sudo add-apt-repository ppa:unit193/encryption
 sudo apt-get -y update
 sudo apt install -y veracrypt
 sudo apt-get install -y mysql-server mysql-workbench
-sudo echo "deb https://apt.enpass.io/ stable main" > \
-  /etc/apt/sources.list.d/enpass.list
-wget -O - https://apt.enpass.io/keys/enpass-linux.key | apt-key add -
+sudo add-apt-repository ppa:jtaylor/keepass
 sudo apt-get update
-sudo apt-get install enpass
+sudo apt-get install -y keepass2
 mv USB-security ~/Desktop
